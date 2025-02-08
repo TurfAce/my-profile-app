a79128ae70040dc45cb1712c37e4f10f1c6e28d2
hoshymo <hoshymo@hazm.in>
2025-02-08 05:04:16 +0000

    Migrate to Vite

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: false,
+    port: 3000,
+  },
+  build: {
+    outDir: 'build'
+  },
+//   base: '/',
+  plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: './src/setupTests.ts',
+//     css: true,
+//     reporters: ['verbose'],
+//     coverage: {
+//         reporter: ['text', 'json', 'html'],
+//         include: ['src/**/*'],
+//         exclude: [],
+//     }
+//   },
+})
